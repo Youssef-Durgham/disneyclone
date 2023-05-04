6eed15ae8f06e8bebfa9a115a982e4bc6fa4cc7f
Youssef Durgham <73782996+Youssef-Durgham@users.noreply.github.com>
2023-05-04 22:52:21 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,12 @@ import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <SessionProvider session={pageProps.session}>
+    <SessionProvider
+      session={pageProps.session}
+      options={{
+        site: "https://main.d2gayeidqn12dv.amplifyapp.com",
+      }}
+    >
       <Component {...pageProps} />
     </SessionProvider>
   );
